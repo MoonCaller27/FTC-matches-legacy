501f0f2c4df972d65d5185ce4783ab7ec7af3047
MoonCaller27 <kadenrwu@gmail.com>
2025-02-11 20:34:27 -0800

    Update clock.js

--- scripts/clock.js
+++ scripts/clock.js
@@ -1,43 +1,57 @@
-const dt = document.getElementById("dt");
-function showDate() {
-  var d = new Date();
-  var hour = d.getHours();
-  var min = d.getMinutes();
-  var sec = d.getSeconds();
-  if (min < 10) min = "0" + min;
-  if (sec < 10) sec = "0" + sec;
-  if (hour > 12) {
-    hour -= 12; 
-  }else if (hour == 0) hour = 12;
+import { useState, useEffect } from "react";
 
-  if(dt.children[0].textContent != Math.trunc(hour/10))
-    dt.children[0].style.color = "#000000";
-  if(dt.children[1].textContent != hour%10)
-    dt.children[1].style.color = "#000000";
-  if(dt.children[2].textContent != Math.trunc(min/10))
-    dt.children[2].style.color = "#000000";
-  if(dt.children[3].textContent != min%10)
-    dt.children[3].style.color = "#000000";
-  if(dt.children[4].textContent != Math.trunc(sec/10))
-    dt.children[4].style.color = "#000000";
-  if(dt.children[5].textContent != sec%10)
-    dt.children[5].style.color = "#000000";
-  var bruh = setTimeout(function(){cont(hour,min,sec)}, 150);
-}
-function cont(hour, min, sec){
-  dt.children[0].textContent=Math.trunc(hour/10);
-  dt.children[1].textContent=hour%10;
-  dt.children[2].textContent=Math.trunc(min/10);
-  dt.children[3].textContent=min%10;
-  dt.children[4].textContent=Math.trunc(sec/10);
-  dt.children[5].textContent=sec%10;
-  dt.children[0].style.color = "#ffc800";
-  dt.children[1].style.color = "#ffc800";
-  dt.children[2].style.color = "#ffc800";
-  dt.children[3].style.color = "#ffc800";
-  dt.children[4].style.color = "#ffc800";
-  dt.children[5].style.color = "#ffc800";
-  var timePulse = setTimeout(function(){showDate()},850);
-}
+const DigitalClock = () => {
+  const [time, setTime] = useState(new Date());
+  const [digits, setDigits] = useState(["0", "0", "0", "0", "0", "0"]);
+  const [colors, setColors] = useState(Array(6).fill("#ffc800"));
 
-showDate();
+  useEffect(() => {
+    const updateClock = () => {
+      let d = new Date();
+      let hour = d.getHours();
+      let min = d.getMinutes();
+      let sec = d.getSeconds();
+
+      if (min < 10) min = "0" + min;
+      if (sec < 10) sec = "0" + sec;
+      if (hour > 12) {
+        hour -= 12;
+      } else if (hour === 0) hour = 12;
+
+      const newDigits = [
+        Math.trunc(hour / 10).toString(),
+        (hour % 10).toString(),
+        Math.trunc(min / 10).toString(),
+        (min % 10).toString(),
+        Math.trunc(sec / 10).toString(),
+        (sec % 10).toString(),
+      ];
+
+      setColors((prevColors) =>
+        prevColors.map((color, index) => (digits[index] !== newDigits[index] ? "#000000" : color))
+      );
+
+      setTimeout(() => {
+        setDigits(newDigits);
+        setColors(Array(6).fill("#ffc800"));
+      }, 150);
+    };
+
+    updateClock();
+    const interval = setInterval(updateClock, 1000);
+
+    return () => clearInterval(interval);
+  }, [digits]);
+
+  return (
+    <div id="dt" style={{ display: "flex", fontSize: "2rem" }}>
+      {digits.map((digit, index) => (
+        <span key={index} style={{ color: colors[index], margin: "0 5px" }}>
+          {digit}
+        </span>
+      ))}
+    </div>
+  );
+};
+
+export default DigitalClock;
